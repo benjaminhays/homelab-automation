223aebbe610fc59edabc6d8c911c587d0c67c736
Ben Hays <ben@benhays.org>
2024-01-27 19:36:45 -0500

    Update compliance scripts

---
+++ Bash/scan-lan.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 (--install-deps) <cidr-range>"
+    exit 1
+fi
+
+if [ "$1" = "--install-deps" ]; then
+    echo "[+] Installing dependencies"
+
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        if lsb_release -a 2>/dev/null | grep -q 'Debian'; then
+            sudo apt-get update && sudo apt-get install -y nmap
+        elif lsb_release -a 2>/dev/null | grep -q 'Fedora'; then
+            sudo dnf install -y nmap
+        elif lsb_release -a 2>/dev/null | grep -q 'Arch\|Manjaro'; then
+            sudo pacman -Syu nmap
+        fi
+    fi
+
+    cidr="$2"
+else
+    cidr="$1"
+fi
+
+sudo nmap -sS -T4 -A $cidr -oN nmap.log
